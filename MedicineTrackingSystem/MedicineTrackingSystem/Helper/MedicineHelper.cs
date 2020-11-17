71ad038f598635446a40ffe3a0290a6cc7ac9ad4
ajaymanav <67000928+ajaymanav@users.noreply.github.com>
2020-11-17 23:36:00 +0530

    Files for medicine tracking system
    Added files for medicine tracking system

---
+++ MedicineTrackingSystem/MedicineTrackingSystem/Helper/MedicineHelper.cs
@@ -0,0 +1,87 @@
+﻿using MedicineTrackingSystem.Models;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace MedicineTrackingSystem.Helper
+{
+    public class MedicineHelper
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public MedicineHelper(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IQueryable<Medicine> GetMedicineData(string sortColumn, string sortColumnDir, string search)
+        {
+            var obj = GetMedicineData();
+
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+            {
+                obj = obj.OrderBy(sortColumn + " " + sortColumnDir);
+            }
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                obj = obj.Where(m => m.MedicineName == search);
+            }
+
+            return obj;
+        }
+
+        private IQueryable<Medicine> GetMedicineData()
+        {
+           var data = GetMedicineJsondata();
+           return data.ToObject<List<Medicine>>().AsQueryable();
+        }
+
+        private JArray GetMedicineJsondata()
+        {
+            string folderAndfileName = "JsonData\\Medicine.json";
+
+            string webRootPath = $"{_hostingEnvironment.ContentRootPath}";
+
+            string newPath = Path.Combine(webRootPath, "wwwroot", folderAndfileName);
+
+
+            string data = string.Empty;
+            JArray jr = new JArray();
+            using (StreamReader r = new StreamReader(newPath))
+            {
+                data = r.ReadToEnd();
+            }
+            if (!string.IsNullOrEmpty(data))
+            {
+                //Read Entire Json file
+                 jr = (JArray)JsonConvert.DeserializeObject(data);
+            }
+
+            return jr;
+        }
+
+        internal bool AddMedicines(Medicine obj)
+        {
+            string folderAndfileName = "JsonData\\Medicine.json";
+
+            string webRootPath = $"{_hostingEnvironment.ContentRootPath}";
+
+            string newPath = Path.Combine(webRootPath, "wwwroot", folderAndfileName);
+
+            var medicineData = GetMedicineJsondata(); 
+
+            string objJson = JsonConvert.SerializeObject(obj,
+                          Formatting.Indented);
+            var newMedicine = JObject.Parse(objJson);
+            medicineData.Add(newMedicine);
+            string newJsonResult = JsonConvert.SerializeObject(medicineData,
+                         Formatting.Indented);
+            File.WriteAllText(newPath, newJsonResult);
+
+            return true;
+        }
+    }
+}
---
+++ MedicineTrackingSystem/MedicineTrackingSystem/Models/Medicine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MedicineTrackingSystem.Models
+{
+    public class Medicine
+    {
+        public string MedicineName { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
