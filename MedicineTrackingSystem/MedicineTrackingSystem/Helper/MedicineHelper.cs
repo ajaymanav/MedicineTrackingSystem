e26c71c25f7a0831edc057845f99db4c6ce56034
ajaymanav <67000928+ajaymanav@users.noreply.github.com>
2020-11-17 23:43:15 +0530

    Updated for editing the medicines notes
    Updated for editing the medicines notes

--- MedicineTrackingSystem/MedicineTrackingSystem/Helper/MedicineHelper.cs
+++ MedicineTrackingSystem/MedicineTrackingSystem/Helper/MedicineHelper.cs
@@ -1,4 +1,4 @@
-﻿using MedicineTrackingSystem.Models;
+using MedicineTrackingSystem.Models;
 using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -9,79 +9,101 @@ using System.Linq.Dynamic.Core;
 
 namespace MedicineTrackingSystem.Helper
 {
-    public class MedicineHelper
-    {
-        private readonly IHostingEnvironment _hostingEnvironment;
-        public MedicineHelper(IHostingEnvironment hostingEnvironment)
-        {
-            _hostingEnvironment = hostingEnvironment;
-        }
-        public IQueryable<Medicine> GetMedicineData(string sortColumn, string sortColumnDir, string search)
-        {
-            var obj = GetMedicineData();
+    public class MedicineHelper
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly string folderAndfileName = "JsonData\\Medicine.json";
+        public MedicineHelper(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IQueryable<Medicine> GetMedicineData(string sortColumn, string sortColumnDir, string search)
+        {
+            var obj = GetMedicineData();
 
-            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
-            {
-                obj = obj.OrderBy(sortColumn + " " + sortColumnDir);
-            }
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+            {
+                obj = obj.OrderBy(sortColumn + " " + sortColumnDir);
+            }
 
-            if (!string.IsNullOrEmpty(search))
-            {
-                obj = obj.Where(m => m.MedicineName == search);
-            }
+            if (!string.IsNullOrEmpty(search))
+            {
+                obj = obj.Where(m => m.MedicineName == search);
+            }
 
-            return obj;
-        }
+            return obj;
+        }
 
-        private IQueryable<Medicine> GetMedicineData()
-        {
-           var data = GetMedicineJsondata();
-           return data.ToObject<List<Medicine>>().AsQueryable();
-        }
+        private IQueryable<Medicine> GetMedicineData()
+        {
+           var data = GetMedicineJsondata();
+           return data.ToObject<List<Medicine>>().AsQueryable();
+        }
 
-        private JArray GetMedicineJsondata()
-        {
-            string folderAndfileName = "JsonData\\Medicine.json";
+        private JArray GetMedicineJsondata()
+        {
+            string newPath = GetJsonFilesPath();
 
-            string webRootPath = $"{_hostingEnvironment.ContentRootPath}";
+            string data = string.Empty;
+            JArray jr = new JArray();
+            using (StreamReader r = new StreamReader(newPath))
+            {
+                data = r.ReadToEnd();
+            }
+            if (!string.IsNullOrEmpty(data))
+            {
+                //Read Entire Json file
+                 jr = (JArray)JsonConvert.DeserializeObject(data);
+            }
 
-            string newPath = Path.Combine(webRootPath, "wwwroot", folderAndfileName);
+            return jr;
+        }
 
+        internal bool AddMedicines(Medicine obj)
+        {
+            string jsonFilePath = GetJsonFilesPath();
 
-            string data = string.Empty;
-            JArray jr = new JArray();
-            using (StreamReader r = new StreamReader(newPath))
-            {
-                data = r.ReadToEnd();
-            }
-            if (!string.IsNullOrEmpty(data))
-            {
-                //Read Entire Json file
-                 jr = (JArray)JsonConvert.DeserializeObject(data);
-            }
+            var medicineData = GetMedicineJsondata();
 
-            return jr;
-        }
+            string objJson = JsonConvert.SerializeObject(obj,
+                          Formatting.Indented);
+            var newMedicine = JObject.Parse(objJson);
+            medicineData.Add(newMedicine);
+            string newJsonResult = JsonConvert.SerializeObject(medicineData,
+                         Formatting.Indented);
+            File.WriteAllText(jsonFilePath, newJsonResult);
 
-        internal bool AddMedicines(Medicine obj)
-        {
-            string folderAndfileName = "JsonData\\Medicine.json";
+            return true;
+        }
 
-            string webRootPath = $"{_hostingEnvironment.ContentRootPath}";
+        private string GetJsonFilesPath()
+        {
+            string webRootPath = $"{_hostingEnvironment.ContentRootPath}";
 
-            string newPath = Path.Combine(webRootPath, "wwwroot", folderAndfileName);
+            string jsonFilePath = Path.Combine(webRootPath, "wwwroot", folderAndfileName);
 
-            var medicineData = GetMedicineJsondata(); 
+            return jsonFilePath;
+        }
 
-            string objJson = JsonConvert.SerializeObject(obj,
-                          Formatting.Indented);
-            var newMedicine = JObject.Parse(objJson);
-            medicineData.Add(newMedicine);
-            string newJsonResult = JsonConvert.SerializeObject(medicineData,
-                         Formatting.Indented);
-            File.WriteAllText(newPath, newJsonResult);
+        internal bool UpdateMedicines(Medicine obj)
+        {
+            string newPath = GetJsonFilesPath();
 
-            return true;
-        }
-    }
+            var medicineData = GetMedicineData();
+
+            foreach( var p in medicineData.Where(x => x.MedicineName == obj.MedicineName && x.Brand == obj.Brand && x.Price == obj.Price && x.ExpiryDate == obj.ExpiryDate))
+            {
+                p.Notes = obj.Notes;
+            }
+
+            string objJson = JsonConvert.SerializeObject(obj,
+                          Formatting.Indented);
+            var newMedicine = JObject.Parse(objJson);
+            string newJsonResult = JsonConvert.SerializeObject(medicineData,
+                         Formatting.Indented);
+            File.WriteAllText(newPath, newJsonResult);
+
+            return true;
+        }
+    }
 }
